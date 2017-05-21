b78e6e8dac3d082cac33067ba1475a10162cb3e7
dominicmeroux <dmeroux@gmail.com>
2017-05-21 15:29:27 -0700

    Adding full dataset analysis

---
+++ Taxi-Powertrain-Analysis/NYC_Medallion_Powertrains.py
@@ -0,0 +1,33 @@
+# Authorized NYC Medallion Vehicles
+# Dataset downloaded from https://data.cityofnewyork.us/Transportation/Medallion-Vehicles-Authorized/rhe8-mgbb
+# Prior download August 10, 2016 using Spark version 2.0.0
+# Latest download March 3, 2017 using Spark version 2.1.0
+
+df = spark.read.csv('/Volumes/SANDISK128/m8DI5uks.csv', header=True)
+
+VehTypes = df.groupBy("Vehicle Type").count()
+VehTypes.show()
+#+------------+-------+                                                          
+#|Vehicle Type|  count|
+#+------------+-------+
+#|         HYB|1267081|
+#|         LV1|    119|
+#|         CNG|    223|
+#|         DSE|    527|
+#|            | 635431|
+#|         WAV|  69247|
+#+------------+-------+
+
+A = VehTypes.take(VehTypes.count())
+
+TotalCount = df.count()
+
+for i in range(0,len(A)):
+	print str(A[i][0]) + "\t" + str(round((float(A[i][1])/TotalCount)*100,2)) + "%"
+
+#HYB	64.23%
+#LV1	 0.01%
+#CNG	 0.01%
+#DSE	 0.03%
+#   	32.21%
+#WAV	 3.51%
---
+++ Taxi-Powertrain-Analysis/README.md
@@ -0,0 +1,3 @@
+**Prerequisites**: Download data from https://data.cityofnewyork.us/Transportation/Medallion-Vehicles-Authorized/rhe8-mgbb
+
+Must install pyspark. 
